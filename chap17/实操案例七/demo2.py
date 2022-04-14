519be678dc0667cb71ade0545f8d6b4e76b71a39
chendonghui <chendonghui@zhongdihang.com>
2022-04-14 14:36:39 +0800

    提交python代码

--- chap17/实操案例七/demo2.py
+++ chap17/实操案例七/demo2.py
@@ -11,9 +11,15 @@ for item in dict_ticket:
         print(i,end='\t\t\t')
     print() # 换行
 #输入要购买的车次
-train_no=input('请输入要购买的车次:')
-persons=input('请输入乘车人，如果是多人请使用逗号分隔')
-s=f'您已购买了{train_no}次列车'
-s_info=dict_ticket[train_no] #获取车次详细信息
-s+=s_info[0]+' '+s_info[1]+' 开'
+while True:
+    train_no = input('请输入要购买的车次:')
+    if dict_ticket.get(train_no) is None:
+        print('对不起，车次不存在，请重新输入！')
+        continue
+    else:
+        break
+persons = input('请输入乘车人，如果是多人请使用逗号分隔')
+s = f'您已购买了{train_no}次列车  '
+s_info = dict_ticket[train_no]  # 获取车次详细信息
+s += s_info[0] + '  ' + s_info[1] + '开  '
 print(f'{s}请{persons}尽快取走纸制车票。【铁路客服】')
