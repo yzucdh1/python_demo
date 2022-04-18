da29f40822ac7c4d5f1f614ba243ecd3005c722e
chendonghui <chendonghui@zhongdihang.com>
2022-04-18 10:51:23 +0800

    提交python代码

--- chap17/实操案例十四/demo1.py
+++ chap17/实操案例十四/demo1.py
@@ -16,13 +16,10 @@ def order_ticket(row_num,row,column):
     tb=pt.PrettyTable()
     tb.field_names = ['行号', '座位1', '座位2', '座位3', '座位4', '座位5']
     for i in range(row_num):
+        lst = [f'第{i + 1}行', '有票', '有票', '有票', '有票', '有票']
         if int(row)==i+1:
-            lst = [f'第{i + 1}行', '有票', '有票', '有票', '有票', '有票']
             lst[int(column)]='已售'
-            tb.add_row(lst)
-        else:
-            lst = [f'第{i + 1}行', '有票', '有票', '有票', '有票', '有票']
-            tb.add_row(lst)
+        tb.add_row(lst)
     print(tb)
 
 if __name__ == '__main__':
